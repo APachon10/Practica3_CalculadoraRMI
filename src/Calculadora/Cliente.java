298bec5f0e03b4f8c23a2179e1a5701bcca899d7
Alberto Pachon <Pachondestroy95@gmail.com>
2019-05-18 23:25:44 +0200

    Practica Terminada pero no funciona Correctamente

--- src/Calculadora/Cliente.java
+++ src/Calculadora/Cliente.java
@@ -7,17 +7,20 @@ import java.util.Scanner;
 public class Cliente {
 	public static void main(String[] args) {
 		RMICalcInterface calc = null;
-		
 		try {
-			Registry reg = LocateRegistry.getRegistry("localhost",5555);
+			Registry reg = LocateRegistry.getRegistry("localhost",1234);
+			System.out.println("Reg : "+reg);
 			calc = (RMICalcInterface) reg.lookup("Calculadora");
+			System.out.println("calc : "+calc);
 			
 			if (calc==null) {
 				System.out.println("Registro Nulo ");
 			}else {
 				int numero1=0,numero2=0;
 				System.out.print("Introduce el Primer Numero: ");
+				numero1=leerEnteros();
 				System.out.print("Introduce el Segundo Numero: ");
+				numero2 = leerEnteros();
 				System.out.println("============================");
 				System.out.println("Suma : "+calc.suma(numero1, numero2));
 				System.out.println("============================");
@@ -28,6 +31,7 @@ public class Cliente {
 				System.out.println("Division: "+calc.div(numero1, numero2));
 			}
 		} catch (Exception e) {
+			System.out.println("Error!! : "+e);
 			e.printStackTrace();
 		}
 	}
--- src/Calculadora/Servidor.java
+++ src/Calculadora/Servidor.java
@@ -10,22 +10,17 @@ public class Servidor implements RMICalcInterface{
 	//Metodo Main 
 	public static void main(String[] args) {
 		Registry reg = null;
+		Servidor v = new Servidor();
 		System.setProperty("java.rmi.server.hostname","192.168.0.12");
 		try {
-			reg = LocateRegistry.createRegistry(5555);
-		} catch (Exception e) {
-			System.out.println("Error : "+e);
-			e.printStackTrace();
-		}
-		Servidor v = new Servidor();
-		try {
+			reg = LocateRegistry.createRegistry(1234);
 			reg.rebind("Calculadora",
 			(RMICalcInterface) UnicastRemoteObject.exportObject(v, 0));
+			System.out.println("Hola");
 		} catch (Exception e) {
 			System.out.println("Error : "+e);
 			e.printStackTrace();
 		}
-		
 	}
 
 	@Override
